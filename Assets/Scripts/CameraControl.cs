0f0506b4533c3f92995f4eb4bf5315a4ee7f1a37
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2019-11-12 16:21:19 +0100

    .initial

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public KeyCode moveForward;
+    public KeyCode moveBackward;
+    public KeyCode rotateRight;
+    public KeyCode rotateLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetKeyInputs();
+    }
+
+    public void GetKeyInputs()
+    {
+        if (Input.GetKeyDown(rotateLeft))
+        {
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, -.5f, 0);
+        }
+
+        if (Input.GetKeyUp(rotateLeft))
+        {
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(rotateRight))
+        {
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, .5f, 0);
+        }
+
+        if (Input.GetKeyUp(rotateRight))
+        {
+            GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(moveForward))
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(0, 0, 100);
+        }
+
+        if (Input.GetKeyUp(moveForward))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(moveBackward))
+        {
+            GetComponent<Rigidbody>().AddRelativeForce(0, 0, -100);
+        }
+
+        if (Input.GetKeyUp(moveBackward))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour
+{
+
+    public Transform grassblock;
+    public Transform clayblock;
+    public int zMod = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int xPos = -5; xPos < 6; ++xPos)
+        {
+            for(int zPos = -5; zPos < 10; ++zPos)
+            {
+                Instantiate(clayblock, new Vector3(xPos, -1, zPos), clayblock.rotation);
+                Instantiate(grassblock, new Vector3(xPos, 0, zPos), grassblock.rotation);
+                
+            }
+            
+        }
+
+        for (int xPos = -5; xPos < 6; ++xPos)
+        {
+            for (int zPos = -3; zPos < 9; ++zPos)
+            {
+                Instantiate(grassblock, new Vector3(xPos, 1, zPos), grassblock.rotation);
+
+            }
+
+        }
+
+        for (int xPos = -5; xPos < 6; ++xPos)
+        {
+            for (int zPos = -2; zPos < 8; ++zPos)
+            {
+                Instantiate(grassblock, new Vector3(xPos, 2, zPos), grassblock.rotation);
+
+            }
+
+        }
+
+        for (int xPos = -5; xPos < 6; ++xPos)
+        {
+            for (int zPos = -1; zPos < 7; ++zPos)
+            {
+                Instantiate(grassblock, new Vector3(xPos, 3, zPos), grassblock.rotation);
+
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
